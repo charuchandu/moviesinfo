6d46f0bcda46d04387305347672bd87d88896ff4
charuchandu <charuchandu88908@gmail.com>
2022-09-07 00:22:46 +0530

    login page with validation is done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  hide = true;
-  constructor() { }
+  form: FormGroup = new FormGroup({});
+  flag: boolean = true;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      email: [null, [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  saveDetails(form: any) {
+    console.log(form.value)
   }
   
 
